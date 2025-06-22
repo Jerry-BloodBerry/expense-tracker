bba267c44071754a3da1b587babe21cf1d6f3e5d
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2025-06-22 16:12:13 +0200

    wip: move form to separate component, use reactive forms

--- client/src/app/features/shared/components/create-expense-dialog/create-expense-dialog.component.ts
+++ client/src/app/features/shared/components/create-expense-dialog/create-expense-dialog.component.ts
@@ -1,83 +1,25 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputNumber } from 'primeng/inputnumber';
 import { SelectModule } from 'primeng/select';
-import { FormsModule } from '@angular/forms';
-import { LocalStorageService } from '../../../../core/services/local-storage.service';
-import { FloatLabel } from 'primeng/floatlabel';
-import { TextareaModule } from 'primeng/textarea';
-import { CURRENCIES, Currency } from '../../../../shared/utils/currencies.util';
-import { ExpenseService } from '../../../../core/services/expense.service';
-import { Category } from '../../../../shared/models/category';
-import { DatePicker } from 'primeng/datepicker';
-import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { CreateExpenseFormComponent } from "../create-expense-form/create-expense-form.component";
 
 @Component({
   selector: 'app-create-expense-dialog',
   imports: [
     Dialog,
     ButtonModule,
-    DatePicker,
-    InputTextModule,
-    FloatLabel,
-    FormsModule,
-    InputNumber,
     SelectModule,
-    TextareaModule,
-    ToggleSwitchModule
-  ],
+    CreateExpenseFormComponent
+],
   templateUrl: './create-expense-dialog.component.html',
   styleUrl: './create-expense-dialog.component.scss'
 })
-export class CreateExpenseDialogComponent implements OnInit {
-    expenseService = inject(ExpenseService);
-    currencies = CURRENCIES;
+export class CreateExpenseDialogComponent {
     visible: boolean = false;
-
-    amount: number = 0;
-    description: string | null = null;
-    selectedCategory?: Category;
-    selectedCurrency!: Currency;
-
-    categoryFilterValue: string = '';
-    showCreateCategory: boolean = false;
-
-    date: Date | undefined;
-    isRecurring: boolean = false;
-
-
-    constructor(private localStorageService: LocalStorageService) {}
-
-    ngOnInit() {
-      this.loadLastUsedCurrency();
-    }
-
-    loadLastUsedCurrency() {
-      const lastUsedCurrencyCode = this.localStorageService.getLastUsedCurrency();
-      this.selectedCurrency = CURRENCIES.find(c => c.code === lastUsedCurrencyCode) || CURRENCIES[0];
-    }
-
-    onCurrencyChange() {
-      this.localStorageService.setLastUsedCurrency(this.selectedCurrency.code);
-    }
-
-    saveExpense() {
-
-    }
 
     showDialog() {
       this.visible = true;
     }
 
-    createCategory(categoryName: string) {
-      // TODO: Implement category creation logic
-      console.log('Creating category:', categoryName);
-    }
-
-    filterCallback(event: any) {
-      this.categoryFilterValue = event.filter;
-      this.showCreateCategory = !this.expenseService.categories.some(cat => cat.name.toLowerCase().startsWith(this.categoryFilterValue.toLowerCase()));
-    }
 }
---
+++ client/src/app/features/shared/components/create-expense-form/create-expense-form.component.ts
@@ -0,0 +1,93 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DatePicker } from 'primeng/datepicker';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputNumber } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { ExpenseService } from '../../../../core/services/expense.service';
+import { LocalStorageService } from '../../../../core/services/local-storage.service';
+import { CURRENCIES, Currency } from '../../../../shared/utils/currencies.util';
+import { Category } from '../../../../shared/models/category';
+import { ButtonModule } from 'primeng/button';
+import { SelectModule } from 'primeng/select';
+import { TextareaModule } from 'primeng/textarea';
+import { ToggleSwitch } from 'primeng/toggleswitch';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-create-expense-form',
+  imports: [
+    ReactiveFormsModule,
+    ButtonModule,
+    RippleModule,
+    DatePicker,
+    InputTextModule,
+    FloatLabel,
+    FormsModule,
+    InputNumber,
+    SelectModule,
+    TextareaModule,
+    ToggleSwitch
+  ],
+  templateUrl: './create-expense-form.component.html',
+  styleUrl: './create-expense-form.component.scss'
+})
+export class CreateExpenseFormComponent implements OnInit {
+  private formBuilder = inject(NonNullableFormBuilder);
+  private localStorageService = inject(LocalStorageService);
+  expenseService = inject(ExpenseService);
+  currencies = CURRENCIES;
+
+  categoryFilterValue: string = '';
+  showCreateCategory: boolean = false;
+
+  expenseForm = this.formBuilder.group({
+    title: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(200)]],
+    category: [null as Category | null, [Validators.required]],
+    description: ['', [Validators.maxLength(1000)]],
+    currency: [null as Currency | null, [Validators.required]],
+    amount: [0.00, [Validators.required, Validators.min(0)]],
+    date: [null as Date | null, [Validators.required]],
+    isRecurring: [false, [Validators.required]]
+  });
+
+  ngOnInit(): void {
+    this.loadLastUsedCurrency();
+  }
+
+  onSubmit() {
+    // TODO: Implement onSubmit logic for expense
+    console.warn(this.expenseForm.value);
+  }
+
+  createCategory(categoryName: string) {
+    // TODO: Implement category creation logic
+    console.log('Creating category:', categoryName);
+  }
+
+  filterCallback(event: any) {
+    this.categoryFilterValue = event.filter;
+    this.showCreateCategory = !this.expenseService.categories.some(
+      cat => cat.name.toLowerCase().startsWith(this.categoryFilterValue.toLowerCase())
+    );
+  }
+
+  loadLastUsedCurrency() {
+    const lastUsedCurrencyCode = this.localStorageService.getLastUsedCurrency();
+    this.expenseForm.patchValue({ currency: CURRENCIES.find(c => c.code === lastUsedCurrencyCode) || CURRENCIES[0]});
+  }
+
+  onCurrencyChange() {
+    if (this.expenseForm.value.currency) {
+      this.localStorageService.setLastUsedCurrency(this.expenseForm.value.currency.code);
+    }
+  }
+
+  handleCancel(event: any) {
+    console.log("Cancel button clicked!");
+  }
+
+  get title() {
+    return this.expenseForm.get('title');
+  }
+}
